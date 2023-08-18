fdd52907929ecfcc88f509c16693e9c5b89eb4ab
MiltonMG <milton.elsalvador26@gmail.com>
2023-08-18 09:57:05 -0600

    preparacion para deployar en vercel

--- controllers/diplomas.controller.js
+++ controllers/diplomas.controller.js
@@ -7,13 +7,13 @@ const diplomasGetById = async(req = request, res = response) => {
     
     //Obteniendo id para realizar GET por id
     const { dui } = req.params;
-    console.log(dui);
+    // console.log(dui);
 
     //Obteniendo la informacion de la base de datos
     try {
         const diploma = await Diploma.findOne({dui});
         
-        console.log(diploma);
+        // console.log(diploma);
 
         //Mensaje de respuesta
         if (diploma) {
@@ -36,6 +36,8 @@ const diplomasGetById = async(req = request, res = response) => {
 
 
 }
+
+
 
 module.exports = {
     diplomasGetById
